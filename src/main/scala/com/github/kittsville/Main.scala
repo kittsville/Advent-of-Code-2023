696a50fac34fea52f312f588467cbdfa451ccf90
Kit <kittsville@googlemail.com>
2025-01-29 11:03:38 +0000

    Add loading puzzle input from file

--- src/main/scala/com/github/kittsville/Main.scala
+++ src/main/scala/com/github/kittsville/Main.scala
@@ -1,6 +1,7 @@
 package com.github.kittsville
 
 import scala.io.StdIn
+import scala.io.Source
 
 object Main extends App {
   println("Puzzle input:")
@@ -15,9 +16,19 @@ object Main extends App {
   private def multilineInput(limit: Int = 1, count: Int = 0): String = {
     val input = StdIn.readLine()
     input match {
+      case "file"              => fileInput()
       case "" if count < limit => s"\n\n${multilineInput(limit, count + 1)}"
       case ""                  => ""
       case nonEmpty            => s"$input\n${multilineInput(limit, 0)}"
     }
+  }
+
+  private def fileInput(): String = {
+    println("Enter file path:")
+    val path = StdIn.readLine()
+    val bufferedSource = Source.fromFile(path)
+    val input = bufferedSource.getLines().mkString("\n")
+    bufferedSource.close
+    input
   }
 }
